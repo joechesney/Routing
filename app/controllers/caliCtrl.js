3eaa50b1ec8b465827fd76e3fb4a8a302252800a
joechesney <33613119+joechesney@users.noreply.github.com>
2018-01-26 15:15:44 -0600

    got that sweet sweet data to print all over the screen

--- app/controllers/caliCtrl.js
+++ app/controllers/caliCtrl.js
@@ -1,8 +1,20 @@
 "use strict";
 
-angular.module("RoutingApp").controller("CaliCtrl", function($scope){
+angular.module("RoutingApp").controller("CaliCtrl", function($scope, $q, $http){
+  $scope.getInfo = ()=>{
+    return $q((resolve, reject)=>{
+      $http.get("../../highways.json")
+      .then(datas=>{
+        // console.log('datas',datas);
+        resolve(datas);
+      });
+    });
+  };
 
-  $scope.highwayName = "etestdgdfCALICALICLIAC";
-
-  
+  $scope.getInfo()
+  .then(highwayDataRaw=>{
+    $scope.highwayName = highwayDataRaw.data.highway2.name;
+    $scope.highwayDescription = highwayDataRaw.data.highway2.description;
+    console.log('$scope.highwayName',$scope.highwayName);
+  });
 });
